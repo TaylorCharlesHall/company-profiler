afb4e7b08bfd08ae97a4f4249559ef6d497d2453
TaylorCharlesHall <139002121+TaylorCharlesHall@users.noreply.github.com>
2023-11-24 13:49:39 -0500

    Add files via upload

---
+++ routing.js
@@ -0,0 +1,7 @@
+//load page and inject into content
+async function loadPage(page) {
+  const res = await fetch(page);
+  const content = await res.text();
+  const element = document.getElementById('content');
+  element.innerHTML = content;
+}
